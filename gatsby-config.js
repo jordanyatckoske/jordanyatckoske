3487c94515ed2d7a9d1963ecebb3f7ccae36a234
Jordan Yatckoske <jordanyatckoske@gmail.com>
2019-11-09 23:22:11 -0600

    proxy requests

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,10 @@ module.exports = {
     description: `I am full stack developer looking for my next opportunity. Look at my work experience and contact me if you think there is a place for me at your organization. Thanks!`,
     author: `@JordanYatckoske`,
   },
-  proxy: "https://api.jordanyatckoske.com",
+  proxy: {
+    prefix: "/api",
+    url: "https://api.jordanyatckoske.com",
+  },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
